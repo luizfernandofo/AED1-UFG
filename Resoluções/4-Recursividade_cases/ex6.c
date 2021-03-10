99207797234f282103a2adc19130119e68670014
Luiz Fernando <60102191+luizfernandofo@users.noreply.github.com>
2021-03-10 16:59:53 -0300

    Update ex6.c

--- Resoluções/4-Recursividade_cases/ex6.c
+++ Resoluções/4-Recursividade_cases/ex6.c
@@ -2,40 +2,37 @@
 #include<stdio.h>
 #include<math.h>
 
+//Retorna a radiciação do número n com precisão "error"
 double my_sqrt(double n, double x);
 
+//Retorna o módulo de x
 double modulo(double x);
 
 int main(int argc, char *argv[]){
 
-    double n, resp;
+    double n;
 
     printf("Qual numero voce deseja calcular a raiz quadrada?\n");
 
     scanf("%lf", &n);
 
-    resp = my_sqrt(n, 1); //retorno está dando errado: 1.#NAN
-
-    printf("Raiz quadrada de %.3lf = %lf\n", n, resp);
+    printf("Raiz quadrada de %.4lf = %.4lf\n", n, my_sqrt(n, 1)); //tive retornos: 1.#NAN
 
     return 0; 
 }
 
 //implementação das funções
 
-double my_sqrt(double n, double x){
+double my_sqrt(double n, double x){ 
 
     double error = 0.0001;
 
-    printf("%.15lf\n", x);
     if(modulo(n - (x*x)) <= error) return x; //condição de parada
 
     x = (x/2.0) + (n/(2.0*x));
     
-    //system("pause");
     my_sqrt(n, x);
     
-    printf("pos: %.15lf\n", x);
 }
 
 double modulo(double x){
