de5b2822183a9ab5d624ce8c4a09d7fecad8ba21
Luiz Fernando <60102191+luizfernandofo@users.noreply.github.com>
2021-05-04 16:52:51 -0300

    update
    Resolução da questão 3

--- Resoluções/8 - Pequisa Sequencial e Binária/ex3.c
+++ Resoluções/8 - Pequisa Sequencial e Binária/ex3.c
@@ -72,14 +72,14 @@ vetIndices binario(int vet[], int tam, int v){
 
     int begin = 0;
     int end = tam - 1;
-    int i;
+    int i, left_i, right_i;
     vetIndices v1;
     int *temp=NULL; 
 
     v1.vet = NULL;
     v1.tam = 0;
 
-    while (begin < end){
+    while (begin <= end){
         i = (begin + end) / 2;
 
         if(vet[i] == v){
@@ -96,11 +96,57 @@ vetIndices binario(int vet[], int tam, int v){
                 exit(1);
             }
             
+            right_i = i + 1;
+            left_i = i - 1;
+
+            while (right_i < tam-1)
+            {
+                if(vet[right_i] == v){
+                    v1.tam++;
+                    temp = (int *) realloc((void *) v1.vet, sizeof(int)*v1.tam);
+                    if(temp != NULL){
+                        v1.vet = temp;
+                        temp = NULL;
+                        v1.vet[v1.tam-1] = right_i;
+
+                    }else{
+                        printf("Falha na realocacao do vetor de indices binario.\n");
+                        exit(1);
+                    }
+
+                }else break;
+
+                right_i++;
+            }
+
+            while (left_i >= 0)
+            {
+                if(vet[left_i] == v){
+                    v1.tam++;
+                    temp = (int *) realloc((void *) v1.vet, sizeof(int)*v1.tam);
+                    if(temp != NULL){
+                        v1.vet = temp;
+                        temp = NULL;
+                        v1.vet[v1.tam-1] = left_i;
+
+                    }else{
+                        printf("Falha na realocacao do vetor de indices binario.\n");
+                        exit(1);
+                    }
+
+                }else break;
+
+                left_i--;
+            }
+            
+            return v1;
+
         }
         
         if(vet[i] < v) begin = i + 1;
         else end = i;
         
+        if(begin==end && end==i) break;
     }
 
     return v1;
@@ -132,15 +178,18 @@ int main(void){
     printf("Ordenando vetor...\n");
     printf("\n");
 
+    printf("<");
+    for(i=0; i<tam-1; i++) printf("%d, ", vet[i]);
+    printf("%d>\n\n", vet[tam-1]);
+
     printf("Valor a ser procurado no vetor: ");
     scanf("%d", &v);
 
     printf("\nSequencial:\n");
     seq = sequencial(vet, tam, v);
 
-    quickSort(seq.vet, 0, seq.tam);
-
     if(seq.vet != NULL){
+        quickSort(seq.vet, 0, seq.tam);
         printf("Indices contendo o valor %d:\n<", v);
         for(i=0; i<seq.tam-1; i++) printf("%d, ", seq.vet[i]);
         printf("%d>\n\n", seq.vet[seq.tam-1]);
@@ -149,18 +198,12 @@ int main(void){
     printf("Binario:\n");
     bin = binario(vet, tam, v);
 
-    quickSort(bin.vet, 0, bin.tam);
-
     if(bin.vet != NULL){
+        quickSort(bin.vet, 0, bin.tam);
         printf("Indices contendo o valor %d:\n<", v);
         for(i=0; i<bin.tam-1; i++) printf("%d, ", bin.vet[i]);
         printf("%d>\n", bin.vet[bin.tam-1]);
-    }else printf("Valor nao encontrado no vetor.\n");
-
-    /*for(i=0; i<tam; i++){
-        printf("%d, ", vet[i]);
-    }
-    printf("\n");*/
+    }else printf("Valor nao encontrado no vetor.\n\n");
 
     free(vet);
     free(seq.vet);
